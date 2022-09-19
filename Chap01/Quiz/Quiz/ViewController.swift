b036d2fba926d4384d110d4a9435a96323260219
birdcagedout <muzom97@gmail.com>
2022-09-19 22:17:16 +0900

    20220919_work

---
+++ Chap01/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 김재형 on 2022/09/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet var questionLabel: UILabel!
+	@IBOutlet var answerLabel: UILabel!
+	
+	@IBAction func showNextQuestion(_ sender: UIButton) {
+		
+	}
+	
+	@IBAction func showAnswer(_ sender: UIButton) {
+		
+	}
+}
+
