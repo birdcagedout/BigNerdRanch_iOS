3af9d9bb0f3e59bbb7b34d9dbb8300331f86f1fc
birdcagedout <muzom97@gmail.com>
2022-09-23 01:32:04 +0900

    20220922_home

--- Chap03/WorldTrotter 복사본/WorldTrotter/ViewController.swift
+++ Chap03/WorldTrotter 복사본/WorldTrotter/ViewController.swift
@@ -22,13 +22,29 @@ class ViewController: UIViewController {
 //		secondView.backgroundColor = UIColor.green
 //		firstView.addSubview(secondView)
 		
-		let layer1: CALayer = CALayer()
-		layer1.frame = .init(x: 10, y: 10, width: 100, height: 100)
-		layer1.backgroundColor = UIColor.red.cgColor
-		view.addSubview(layer1)
 		
-		let layer2: CALayer = CALayer()
-		layer2.frame = .init(
+		let colors: [CGColor] = [
+			.init(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1),
+			.init(red: 0.4508578777, green: 0.9882974029, blue: 0.8376303315, alpha: 1),
+			.init(red: 0.476841867, green: 0.5048075914, blue: 1, alpha: 1)
+		]
+		let changeColors: [CGColor] = [
+			.init(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+			.init(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),
+			.init(red: 0, green: 0.9914394021, blue: 1, alpha: 1)
+		]
+
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.frame  = self.view.bounds
+		gradientLayer.colors = colors
+		self.view.layer.addSublayer(gradientLayer)
+		
+		let colorAnimation = CABasicAnimation(keyPath: "colors")
+		colorAnimation.toValue = changeColors
+		colorAnimation.duration = 3
+		colorAnimation.autoreverses = true
+		colorAnimation.repeatCount = .infinity
+		gradientLayer.add(colorAnimation, forKey: "colorChangeAnimation")
 	}
 
 }
