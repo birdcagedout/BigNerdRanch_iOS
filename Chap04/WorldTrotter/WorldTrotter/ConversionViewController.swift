23c44a05c71a475ead2f2ecc9f61c5b458156716
birdcagedout <muzom97@gmail.com>
2022-09-26 21:29:08 +0900

    20220926_work

--- Chap04/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Chap04/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
 	
 	@IBOutlet var textField: UITextField!
 	@IBOutlet var celsiusLabel: UILabel!
 	
+	// 저장 프로퍼티 fahrenheitValue + 감시자
 	var fahrenheitValue: Measurement<UnitTemperature>? {
 		didSet {
 			updateCelsiusLabel()
 		}
 	}
 	
+	// 연산 프로퍼티 celsiusValue
 	var celsiusValue: Measurement<UnitTemperature>? {
 		if let fahrenheitValue = fahrenheitValue {
 			return fahrenheitValue.converted(to: .celsius)
@@ -26,6 +28,16 @@ class ConversionViewController: UIViewController {
 		}
 	}
 	
+	// numberFormatter 프로퍼티(클로저)
+	let numberFormatter: NumberFormatter = {
+		let nf = NumberFormatter()
+		nf.numberStyle = .decimal
+		nf.minimumFractionDigits = 0
+		nf.maximumFractionDigits = 1
+		return nf
+	}()
+	
+	// view가 로드되면 실행(초기화)
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		updateCelsiusLabel()
@@ -33,6 +45,7 @@ class ConversionViewController: UIViewController {
 		print("ConversionViewController loaded it view.")
 	}
 	
+	// 텍스트필드 값이 바뀌면 ==> 저장 프로퍼티 fahrenheitValue
 	@IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
 		if let textinput = textField.text, let value = Double(textinput) {
 			fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
@@ -41,17 +54,28 @@ class ConversionViewController: UIViewController {
 		}
 	}
 	
+	// 텍스트필드 외 클릭하면 키보드 없애기
 	@IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
 		textField.resignFirstResponder()
 	}
 	
+	// 연산 프로퍼티 celsiusValue값 ==> Label값
 	func updateCelsiusLabel() {
 		if let celsiusValue = celsiusValue {
-			celsiusLabel.text = "\(Int(celsiusValue.value))"					// Double형은 소수점 때문에 Int형으로 cast
+			celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
 		} else {
 			celsiusLabel.text = "???"
 		}
 	}
-
+	
+	// UITextFieldDelegate 프로토콜 구현
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		let existingTextSeparator = textField.text?.ranges(of: ".")
+		let newTextSeparator = string.ranges(of: ".")
+		if existingTextSeparator != nil && newTextSeparator != nil {
+			return false
+		} else {
+			return true
+		}
+	}
 }
-
